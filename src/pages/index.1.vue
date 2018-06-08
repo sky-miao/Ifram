0e7630097644617963e6d1d33958dc5c69fde56d
sky_miao <mengmiao_sky@126.com>
2018-06-08 14:57:20 +0800

    menu

---
+++ src/pages/index.1.vue
@@ -0,0 +1,109 @@
+<template>
+  <el-row class="main-page">
+    <el-col :xs="0" :span="2" class="main-el-row">
+      <el-menu
+        default-active="1"
+        class="main-nav-page"
+        background-color="#05172f"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <template>
+          <el-menu-item>
+            <img src="../assets/imgs/logo.png" alt="logo">
+          </el-menu-item>
+          <el-menu-item :index="menuItem.id" v-for="menuItem in menuList" :key="menuItem.id">
+            <span class="item" slot="title" @click="selectMenu(menuItem.url)">{{menuItem.menuName}}</span>
+          </el-menu-item>
+        </template>
+      </el-menu>
+    </el-col>
+    <el-col :xs="24" :lg="0" class="main-el-row-sm">
+      <img src="../assets/imgs/logo.png" alt="logo">
+      <span class="sm-nav-wrap">
+        <img v-if="menuVisible === false" @click="clickMenu" src="../assets/imgs/nav.png"/>
+        <i v-else @click="clickMenu" class="el-icon-close"></i>
+      </span>
+    </el-col>
+    <ul v-show="menuVisible" class="nav-ul-wrap">
+      <li :index="menuItem.id" v-for="menuItem in menuList" :key="menuItem.id">
+        <span class="item" slot="title" @click="selectMenu(menuItem.url)">{{menuItem.menuName}}</span>
+      </li>
+    </ul>
+    <el-col :xs="24" :span="22" class="main-el-row">
+      <div class="main-con-page">
+        <iframe style="width: 100%; height: 99%" :src="url"></iframe>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import { menuList } from './url'
+export default {
+  data () {
+    return {
+      menuVisible: false,
+      menuList: menuList,
+      url: 'http://localhost:3001/#/'
+    }
+  },
+  components: {
+  },
+  methods: {
+    selectMenu (url) {
+      this.url = url
+    },
+    clickMenu () {
+      this.menuVisible = !this.menuVisible
+    }
+  }
+}
+</script>
+<style lang="less">
+  .main-page{
+    height: 100%;
+    .main-el-row{
+      height: 100%;
+    }
+    .main-nav-page{
+      background: #ddd;
+      height: 100%;
+      min-width: 150px;
+      border-right: none;
+      .item{
+        display: inline-block;
+        width: 100%;
+      }
+    }
+    .main-el-row-sm{
+      position: relative;
+      background: #ddd;
+      height: 40px;
+      img{
+        position: absolute;
+        top: 50%;
+        left: 20px;
+        transform: translateY(-50%);
+      }
+      .sm-nav-wrap{
+        position: absolute;
+        top: 50%;
+        right: 20px;
+        transform: translateY(-50%);
+      }
+    }
+    .nav-ul-wrap{
+      background: #333;
+      li{
+        border-bottom: 1px solid #ddd;
+      }
+      .item{
+        display: inline-block;
+        width: 100%;
+      }
+    }
+    .main-con-page{
+      background: #666;
+      height: 100%;
+    }
+  }
+</style>
--- src/pages/url.js
+++ src/pages/url.js
@@ -2,18 +2,98 @@ module.exports = {
   menuList: [
     {
       id: '1',
-      menuName: '1',
-      url: 'www.baidu.com'
+      menuName: 'shard-01',
+      url: 'https://www.baidu.com/'
     },
     {
       id: '2',
-      menuName: '2',
-      url: 'www.xinlang.com'
+      menuName: 'shard-02',
+      url: 'https://weibo.com/'
     },
     {
       id: '3',
-      menuName: '3',
-      url: 'www.google.com'
+      menuName: 'shard-03',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '4',
+      menuName: 'shard-04',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '5',
+      menuName: 'shard-05',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '6',
+      menuName: 'shard-06',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '7',
+      menuName: 'shard-07',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '8',
+      menuName: 'shard-08',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '9',
+      menuName: 'shard-09',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '10',
+      menuName: 'shard-10',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '12',
+      menuName: 'shard-12',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '13',
+      menuName: 'shard-13',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '14',
+      menuName: 'shard-14',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '15',
+      menuName: 'shard-15',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '16',
+      menuName: 'shard-16',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '17',
+      menuName: 'shard-17',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '18',
+      menuName: 'shard-18',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '19',
+      menuName: 'shard-19',
+      url: 'https://www.baidu.com/'
+    },
+    {
+      id: '20',
+      menuName: 'shard-20',
+      url: 'https://www.baidu.com/'
     }
   ]
 }
